5888f6bf0eff7ece63425fa1a76b0c0df0a93453
leorvergani <vergani@outlook.com.br>
2024-09-21 00:00:12 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,8 @@ function App() {
   },
 ])
 
-function onTaskClick() {
-  const newTasks = taks.map((task) => {
+function onTaskClick(taskId) {
+  const newTasks = tasks.map((task) => {
     // Atualizar tarefa
     if (task.id == taskId) {
       return { ...task, isCompleted: !task.isCompleted };
@@ -43,7 +43,7 @@ function onTaskClick() {
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
       <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
-      <Tasks tasks={tasks}/>
+      <Tasks tasks={tasks} onTaskClick={onTaskClick}/>
       <AddTasks />
 
       </div>
